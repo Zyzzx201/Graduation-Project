3708336b9329738aaee8dfbf95d1c0647c8b50ec
AndrewUsamaNassef <46010408+AndrewUsamaNassef@users.noreply.github.com>
2019-05-14 21:28:34 +0200

    Add files via upload

---
+++ src/main/java/com/example/mac/gradproj2/BackgroundWorker2.java
@@ -0,0 +1,90 @@
+package com.example.mac.gradproj2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundWorker2 extends AsyncTask<String , Void , String> {
+        Context context;
+        AlertDialog alertDialog;
+        Login login;
+
+        BackgroundWorker2(Context ctx, Login x)
+        {
+        context = ctx;
+        login = x;
+        }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String login_url = "http://192.168.1.103/webapp/login.php";
+        if (type.equals("login")) {
+            try {
+                String username = params[1];
+                String password = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS, "UTF-8"));
+                String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8") + "&" +
+                        URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8") + "&" ;
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine())!= null)
+                {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage("Welcome");
+        alertDialog.show();
+        if(!result.equals("failed"))
+        {
+            login.image(result);
+        }
+    }
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
---
+++ src/main/java/com/example/mac/gradproj2/Login.java
@@ -0,0 +1,48 @@
+package com.example.mac.gradproj2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+    EditText username,password;
+    String uid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        //value from another form
+        //Bundle bd= getIntent().getExtras();
+        //username1= bd.getString("username");
+        username = (EditText)findViewById(R.id.ET_username);
+        password = (EditText)findViewById(R.id.ET_pass);
+    }
+
+    public void onlog(View view) {
+        String str_name = username.getText().toString();
+        String str_password = password.getText().toString();
+        String type = "login";
+        BackgroundWorker2 backgroundWorker = new BackgroundWorker2(this,this);
+        backgroundWorker.execute(type,str_name,str_password);
+        //System.err.println("user id"+uid);
+        //Toast.makeText(getApplicationContext(),uid, Toast.LENGTH_SHORT).show();
+
+    }
+    public void image(String id)
+    {
+        uid = id;
+        Intent ret = new Intent(Login.this, Image.class);
+        ret.putExtra("u_id", uid);
+        startActivity(ret);
+    }
+
+    public void onReg(View view)
+    {
+        Intent ret = new Intent(Login.this, MainActivity.class);
+        startActivity(ret);
+    }
+}
